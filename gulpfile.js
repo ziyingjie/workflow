840dcd54f279e67fecbe87f44f22c56ae935e803
ziyingjie <ziyingjie123@163.com>
2018-05-17 01:35:10 +0800

    'zz'

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+// gulp.task('less', () => {
+//     gulp.src('src/**/*.less')
+//         .pipe(less())
+//         .pipe(autoprefixer({
+//             browsers: ['last 5 versions', 'Firefox > 20'],
+//             cascade: false
+//         }))
+//         .pipe(cleanCSS())
+//         .pipe(gulp.dest('build'));
+// });
+
+gulp.task('clean', () => {
+    del.sync('build');
+});
+
+gulp.task('less', () => {
+    gulp.src('src/**/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'Firefox > 20'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build'));
+});
+
+
+gulp.task('default', ['less'], () => {
+    console.log('done!');
+});
+
+
+gulp.task('watch', () => {
+    const watcher = gulp.watch('src/**/*', ['default']);
+    watcher.on('change', event => {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
---
+++ src/script/util.js
@@ -0,0 +1,9 @@
+export function fn1() {
+  console.log('This is fn1');
+};
+
+export function fn2() {
+  console.log('This is fn2');
+};
+
+Object.assign({ a: 1 }, { b: 2 })
